22ebfdb4631429933457271947c6c32eab27cc2d
nobo <505931977@qq.com>
2018-12-09 22:36:51 +0800

    Add        .eslintrc.json Add        .gitignore Add        package-lock.json Add        package.json Add        src/components/WidgetsPanel.js Add        src/components/WidgetsPanel/WidgetsList.js Add        src/style/components.scss

---
+++ src/components/WidgetsPanel.js
@@ -0,0 +1,15 @@
+/**
+ * 页面布局设计器-部件列表
+ * author: nobo.zhou
+ * date: 2018.12.09
+ */
+
+import React from 'react';
+
+export default class WidgetsPanel extends React.Component {
+
+    render() {
+
+    }
+
+}
---
+++ src/components/WidgetsPanel/WidgetsList.js
@@ -0,0 +1,27 @@
+
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class WidgetsList extends React.Component {
+
+    static propTypes = {
+        items: PropTypes.array
+    }
+
+    static defaultProps = {
+        items: []
+    }
+
+
+
+    render() {
+
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
